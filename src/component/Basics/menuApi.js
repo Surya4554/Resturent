ead669e242183bde42e7a6a9264641f5217d193f
Suraj Kumar Prajapati <95536336+Surya4554@users.noreply.github.com>
2022-04-03 11:51:54 +0530

    first commit

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,333 @@
+const Menu = [
+   {
+       id: 1,
+       image: "images/burger.jpg",
+       name: "Burger",
+       category: "evening",
+       price: "Rs. 120",
+       description: "I lovr Burger realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+   },
+   {
+       id: 2,
+       image: "images/chicken.jfif",
+       name: "Chicken",
+       category: "evening",
+       price: "Rs. 140",
+       description: "i like Chicken and what about you aly oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+   },
+   {
+       id: 3,
+       image: "images/dhokala.jpg",
+       name: "Dhokala",
+       category: "lunch",
+       price: "Rs. 45",
+       description: "I am interested in yiur food  realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+   },
+   {
+       id: 4,
+       image: "images/idali.jpg",
+       name: "Idali",
+       category: "breakfast",
+       price: "Rs. 70",
+       description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+   },
+   {
+       id: 5,
+       image: "images/macroni.jfif",
+       name: "Macroni",
+       category: "breakfast",
+       price: "Rs. 60",
+       description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+   },
+   {
+       id: 6,
+       image: "images/maggi.jpg",
+       name: "Maggi",
+       category: "lunch",
+       price: "Rs. 120",
+       description: "I love Maggi How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+   },
+   {
+       id: 7,
+       image: "images/rice.jfif",
+       name: "Rice",
+       category: "dinner",
+       price: "Rs. 260",
+       description: "I love Rice.Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+   },
+   {
+       id: 8,
+       image: "images/samosa.jpg",
+       name: "Samosa",
+       category: "lunch",
+       price: "Rs. 20",
+       description: "I love Samosa.Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+   },
+   {
+       id: 9,
+       image: "images/sevpuri.jpg",
+       name: "Sevpuri",
+       category: "lunch",
+       price: "Rs. 30",
+       description: "I love Sevpuri.Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+   },
+   {
+       id: 10,
+       image: "images/snack.jpg",
+       name: "Snack",
+       category: "breakfast",
+       price: "Rs. 60",
+       description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+   },
+   {
+    id: 11,
+    image: "images/biharithali.jpg",
+    name: "Special Bihar Thali",
+    category: "lunch",
+    price: "Rs. 160",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 12,
+    image: "images/famousthali.jpg",
+    name: "Resurant Famous Thali",
+    category: "dinner",
+    price: "Rs. 215",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 13,
+    image: "images/gujratithali.jpg",
+    name: "Gujrati Thali",
+    category: "lunch",
+    price: "Rs. 135",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 14,
+    image: "images/maharashtrathali.jpg",
+    name: "Maharashtra Special Thali",
+    category: "lunch",
+    price: "Rs. 120",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 15,
+    image: "images/simplethali.jpg",
+    name: "Simple Food Thali",
+    category: "lunch",
+    price: "Rs. 75",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 16,
+    image: "images/upthali.jpg",
+    name: "UP Famous Thali",
+    category: "lunch",
+    price: "Rs. 80",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 17,
+    image: "images/vegthali.jpg",
+    name: "Pure Veg Thali",
+    category: "lunch",
+    price: "Rs. 110",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 18,
+    image: "images/biryani.jpg",
+    name: "Cude Rice Biryani",
+    category: "dinner",
+    price: "Rs. 260",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 19,
+    image: "images/chicken.jfif",
+    name: "Chicken Rice For Dinner",
+    category: "dinner",
+    price: "Rs. 130",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 20,
+    image: "images/rice.jfif",
+    name: "Fry Rice For Dinner",
+    category: "dinner",
+    price: "Rs. 55",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 21,
+    image: "images/bhajipav.jpg",
+    name: "Delicious Bhaji Pav",
+    category: "evening",
+    price: "Rs. 35",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 22,
+    image: "images/buttersweet.jpg",
+    name: "Delicious Cream Sweet",
+    category: "evening",
+    price: "Rs. 85",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 23,
+    image: "images/dosa1.jpg",
+    name: "Simple Veg Dosa",
+    category: "evening",
+    price: "Rs. 60",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 24,
+    image: "images/gutu.jpg",
+    name: "Dry Sweet in Special",
+    category: "evening",
+    price: "Rs. 25",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 25,
+    image: "images/jalebi.jpg",
+    name: "Famous Indian Jalebi",
+    category: "breakfast",
+    price: "Rs. 150",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 26,
+    image: "images/sonpapdi.jpg",
+    name: "All Types Son Papdi",
+    category: "evening",
+    price: "Rs. 25",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 27,
+    image: "images/springroll.jpg",
+    name: "Veg Spring Roll",
+    category: "evening",
+    price: "Rs. 35",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 28,
+    image: "images/dinner1.jpg",
+    name: "Veg Dinner Set Thali",
+    category: "dinner",
+    price: "Rs. 85",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 29,
+    image: "images/dinner2.jpg",
+    name: "Spicy Food for Dinner",
+    category: "dinner",
+    price: "Rs. 105",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 30,
+    image: "images/dinner3.jpg",
+    name: "Indian Special or Dinner",
+    category: "dinner",
+    price: "Rs. 135",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 31,
+    image: "images/dinner4.jpg",
+    name: "Non Veg Thali For Dinner",
+    category: "dinner",
+    price: "Rs. 100",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 32,
+    image: "images/dinner5.png",
+    name: "Gravy Chicken Food",
+    category: "dinner",
+    price: "Rs. 177",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 33,
+    image: "images/dinner6.jpg",
+    name: "Delicious Pure Veg Food",
+    category: "dinner",
+    price: "Rs. 140",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 34,
+    image: "images/break1.jpg",
+    name: "Breakfast Food",
+    category: "breakfast",
+    price: "Rs. 40",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 35,
+    image: "images/break2.jpg",
+    name: "Delicious Indian Breakfast",
+    category: "breakfast",
+    price: "Rs. 50",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 36,
+    image: "images/break3.jpg",
+    name: "Pure Veg Breakfast",
+    category: "breakfast",
+    price: "Rs. 29",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 37,
+    image: "images/break4.jpg",
+    name: "Bread With Butter Sly",
+    category: "breakfast",
+    price: "Rs. 120",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 38,
+    image: "images/break5.jpg",
+    name: "Delicious Half Fry Egg",
+    category: "breakfast",
+    price: "Rs. 40",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 39,
+    image: "images/break6.jpg",
+    name: "Veg Thrill Snacks",
+    category: "breakfast",
+    price: "Rs. 80",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 40,
+    image: "images/break7.jpg",
+    name: "Delicious Bread With Apple",
+    category: "breakfast",
+    price: "Rs. 65",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+{
+    id: 41,
+    image: "images/break8.jpg",
+    name: "Tomato Egg Salvons",
+    category: "breakfast",
+    price: "Rs. 50",
+    description: "Hii How are youI lovr maggi realy oo yues Lorem ipsum dolor amet constant what you think about maggi. in my house all peoples love maggi and your family about explain this things."
+},
+
+];
+
+export default Menu;
